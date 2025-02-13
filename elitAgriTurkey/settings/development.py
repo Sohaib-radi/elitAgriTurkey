9a543c536fb044f6ef29e77ee4e360bac59ddce7
Sohaib radi <sohaib.radi@gmail.com>
2025-02-13 23:02:39 +0300

    modified:   elitAgriTurkey/settings/development.py

--- elitAgriTurkey/settings/development.py
+++ elitAgriTurkey/settings/development.py
@@ -9,8 +9,12 @@ ALLOWED_HOSTS = ['*']
 # Database configuration for development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elitagri',           
+        'USER': 'adminDb',            
+        'PASSWORD': 'iW}B$i4vBy{BL["}',   
+        'HOST': '	63.250.41.35',           
+        'PORT': '5432',               
     }
 }
 
