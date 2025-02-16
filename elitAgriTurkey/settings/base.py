364ab090e049a2c6f1f005f3b9b9d5bb62dde840
Sohaib radi <sohaib.radi@gmail.com>
2025-02-16 15:41:55 +0300

    modified:   elitAgriTurkey/settings/base.py

--- elitAgriTurkey/settings/base.py
+++ elitAgriTurkey/settings/base.py
@@ -110,9 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-print("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-print(BASE_DIR)
-print("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
